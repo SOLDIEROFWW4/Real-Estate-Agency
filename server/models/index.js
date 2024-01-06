28642107e9e678a817904f24a5b48c5c4d058bc5
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-01-06 21:00:51 +0300

    feat: rewrite index.js

--- server/models/index.js
+++ server/models/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const models = require('./models/models')
 const start = async () => {
     try {
         await sequelize.authenticate();
