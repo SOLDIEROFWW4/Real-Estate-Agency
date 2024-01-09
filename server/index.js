dc3946b0f0c581dbe329a68004f6a25edcf49c66
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-01-09 17:39:55 +0300

    feat: some project files refactored

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("./models/index");
-const models = require('./models/models')
+const models = require('./models/models');
+const router = require('./routers/index')
 
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
@@ -15,7 +16,9 @@ const start = async () => {
         await sequelize.authenticate();
         await sequelize.sync({ alter: true });
         app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-    } catch (e) {
+    } 
+    
+    catch (e) {
         console.log(e);
     }
 };
