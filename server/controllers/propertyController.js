06227d1e09c0ed3a2d72ce46e74b60145b191f49
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-01-09 17:49:50 +0300

    feat: modify and refactored propertyController.js

--- server/controllers/propertyController.js
+++ server/controllers/propertyController.js
@@ -1,10 +1,10 @@
-const { Property } = require('../models/models');
+const { Property, PropertyType } = require('../models/models');
 
 class PropertyController {
-    async getAllProperties(req, res) {
+    async getAll(req, res) {
         try {
             const properties = await Property.findAll();
-
+    
             return res.json(properties);
         }
         catch (err) {
@@ -14,16 +14,33 @@ class PropertyController {
         }
     }
 
-    async getProperty(req, res) {
-        const { propertyId } = req.params;
+    async getCatalogPropertiesByType(req, res) {
+        try {
+            const { propertyTypeId } = req.params;
 
+            const properties = await Property.findAll({
+                where: { IsSold: false, propertyTypeId: propertyTypeId }
+            });
+    
+            return res.json(properties);
+        }
+        catch (err) {
+            console.error(err);
+
+            return res.sendStatus(500).json({ error: 'Ошибка сервера.' });
+        }
+    }
+
+    async getOne(req, res) {
+        const { propertyId } = req.params;
+    
         try {
             const property = await Property.findByPk(propertyId);
-
+    
             if (!property) {
                 return res.status(404).json({ error: 'Собственность не найдена.' });
             }
-
+    
             return res.status(200).json({ property });
         }
         catch (err) {
@@ -38,7 +55,7 @@ class PropertyController {
             const properties = await Property.findAll({
                 where: { isSold: false }
             });
-
+    
             return res.json(properties);
         }
         catch (err) {
@@ -50,14 +67,14 @@ class PropertyController {
 
     async getCatalogProperty(req, res) {
         const { propertyId } = req.params;
-
+    
         try {
             const property = await Property.findByPk(propertyId);
-
+    
             if (!property || property.isSold == true) {
                 return res.status(404).json({ error: 'Собственность не найдена.' });
             }
-
+    
             return res.status(200).json({ property });
         }
         catch (err) {
@@ -69,13 +86,10 @@ class PropertyController {
 
     async createProperty(req, res) {
         try {
-            const property = { ...req.body };
-
-            property.dateCreated = new Date();
-            property.dateUpdated = property.dateCreated;      
-
+            const property = { ...req.body };    
+    
             const createdProperty = await Property.create(property);
-
+    
             return res.status(201).json(createdProperty);
         }
         catch (err) {
@@ -87,22 +101,20 @@ class PropertyController {
 
     async updateProperty(req, res) {
         const { propertyId } = req.params;
-
+    
         const property = { ...req.body };
-
+    
         if (isNaN(propertyId) || parseInt(propertyId) !== property.propertyId) {
             return res.sendStatus(400);
         }
-
+    
         if ((await User.findOne({ where: { propertyId: propertyId } })) == null) {
             return res.sendStatus(404);
         }
-
-        property.dateUpdated = new Date();
-
+    
         try {
             await User.update(property, { where: { propertyId: propertyId } });
-
+    
             return res.sendStatus(204);
         }
         catch (err) {
